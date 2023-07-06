b4b5313a7cde7118f868941173cc2507d839b739
Simone W <simwk07@gmail.com>
2023-07-06 11:39:18 +0100

    removed unecessary files, added header, textInput, and footer components. Added style to TextInput form and toast error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import { Container, Box } from '@chakra-ui/react'
+import Header from './components/Header';
+import TextInput from './components/TextInput';
+import Footer from './components/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [keywords, setKeywords] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const extractKeywords = (text) => {
+    setLoading(true)
+    setIsOpen(true)
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' + text + ''
+      })
+    }
+  }
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box bg='blue.600' color='white' height='100vh' padddingtop={130}>
+      <Container maxWidth='3xl' centerContent>
+        <Header/>
+        <TextInput extractKeywords={extractKeywords}/>
+        <Footer/>
+      </Container>
+    </Box>
   )
 }
 
-export default App
+export default App;
